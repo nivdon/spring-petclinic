c77f2db8235a50038a430ce51c76b81bfb895a89
Niv <niv@b.com>
2021-12-22 04:56:05 +0000

    Student3

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/ec2-user/student
+cd /home/ec2-user/student3
 echo "running the Java application now"
 #java -jar spring-petclinic*.jar > /dev/null 2> /dev/null < /dev/null &
 java -jar spring-petclinic*.jar &
